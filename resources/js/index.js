5333fc165249abc3f4adaa1b28651534c6ebcc4a
lil L <57925562+leolopez10@users.noreply.github.com>
2020-01-15 16:30:31 -0600

    Erased javascript to start over

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,29 +1,71 @@
-let inquirer = require("inqurirer");
+let inquirer = require("inquirer");
 let fs = require("fs");
+let axios = require("axios");
+let generateHTML = require("./generateHTML");
 
 
-inquirer
-    .prompt([{
-        type: "input",
-        messages: "Lets build a profile document, what is your GitHub user name?",
-        name: "userName"
-    }])
-    .then(function(response) {
-        //GitHub API information
-        const queryURL = "https://api.github.com/users/" + userName
-        let userName =
 
-    })
 
-const questions = [
-    "Lets build a profile document, what is your GitHub user name?",
-    "What is your favorite color?"
-];
 
-function writeToFile(fileName, data) {
 
-}
 
-function init() {
 
-    init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let gitHubUserName;
+
+// inquirer
+//     .prompt([{
+//             type: "input",
+//             message: "Hi, let's create a profile document. What is your name?",
+//             name: "name"
+//         },
+//         {
+//             type: "input",
+//             message: "Lets build a profile document, what is your GitHub user name?",
+//             name: "userName"
+//         },
+//         {
+//             type: "rawlist",
+//             message: "What is your favorite color?",
+//             choices: ["Green", "Blue", "Pink", "Red"],
+//             name: "color"
+//         }
+//     ])
+//     .then(function(response) {
+//         //GitHub API information using Axios
+//         axios
+//             .get(queryURL)
+//         var githubUsername = response.username.split(' ').join('') + ".json";
+//         const queryURL = "https://api.github.com/users/" + gitHubUserName;
+//         console.log(queryURL);
+//     })
