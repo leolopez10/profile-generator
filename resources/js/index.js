befc1b105918a5ae184fc6de02b499b2bb2222a8
lil L <57925562+leolopez10@users.noreply.github.com>
2020-01-15 19:59:20 -0600

    collected and saved all the data need from github

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,176 @@
-let inquirer = require("inquirer");
-let fs = require("fs");
-let axios = require("axios");
-let generateHTML = require("./generateHTML");
+//=======================//
+// Node Packet Managment               
+//=======================//
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const generateHTML = require("./generateHTML");
+// const HTMLtoPDF = require("html-pdf")
 
 
+//===========//
+// Promises                            
+//===========//
+
+// const readFileAsync = util.promisify(fs.readFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 
+//========================================================================//
+// These are constant variable that we will need to develop our PDF file                 
+//========================================================================//
+// const profileImg;
+// const userName;
+// const userLocation;
+// const userGitProfile;
+// const userBlog;
+// const userBio;
+// const numberOfRepos;
+// const numberOfFollowers;
+// const numberOfFollowing;
+
+//=======================================================//
+//  Prompt user to get github username and favorite color                 
+//=======================================================//
+
+
+inquirer
+    .prompt([{
+            type: "input",
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            type: "rawlist",
+            message: "pick a color",
+            choices: ["Green", "Blue", "Pink", "Red"],
+            name: "color"
+        }
+    ])
+    .then(function(userInput) {
+        // console.log(userInput);
+        //================================================
+        // create variables for api and store user inputs
+        //=================================================
+        const gitHubLogin = userInput.username;
+        const color = userInput.color;
+        const queryUrl = "https://api.github.com/users/" + gitHubLogin;
+        const queryStarUrl = "https://api.github.com/users/" + gitHubLogin + "/starred";
+
+        //====================================================
+        //Run gitHubData function to retrieve user information
+        //====================================================
+        gitHubData(queryUrl);
+
+        //=====================================================
+        //Run gitHubStars function to get remaining github data
+        //=====================================================
+        gitHubStars(queryStarUrl);
+        // let gitUserNameJSON = userInput.username.split(" ").join("") + ".json"
+        // console.log(gitUserNameJSON);
+
+        // const queryUrl = ""
+        // writeFileAsync(gitHubUserName, JSON.stringify(userInput, null, "\t"), function(err){
+        //     if(err){
+        //         return console.log(err);
+        //     }
+
+        // })
+    });
+
+function gitHubStars(queryStarUrl) {
+    axios
+        .get(queryStarUrl)
+        .then(function(res) {
+            let userStars = res.data.length;
+            console.log(userStars);
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+}
+
+function gitHubData(queryUrl) {
+    axios
+        .get(queryUrl)
+        .then(function(res) {
+
+            let profileImg = res.data.avatar_url + ".png";
+            let userName = res.data.login;
+            let userLocation = res.data.location;
+            let userGitProfile = res.data.html_url;
+            let userBlog = res.data.blog;
+            let userBio = res.data.bio;
+            let numberOfRepos = res.data.public_repos;
+            let numberOfFollowers = res.data.followers;
+            let numberOfFollowing = res.data.following;
+            //missing github stars
+            console.log(profileImg, userName, userLocation, userGitProfile, userBlog, userBio, numberOfRepos, numberOfFollowers, numberOfFollowing);
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+
+
+}
+
+// .then(function({ username }) {
+//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//     axios.get(queryUrl).then(function(res) {
+//         const repoNames = res.data.map(function(repo) {
+//             return repo.name;
+//         });
+
+//         const repoNamesStr = repoNames.join("\n");
+
+//         fs.writeFile("repos.txt", repoNamesStr, function(err) {
+//             if (err) {
+//                 throw err;
+//             }
+
+//             console.log(`Saved ${repoNames.length} repos`);
+//         });
+//     });
+// });
+
+// axios
+//     .get("https://icanhazdadjoke.com/", config)
+//     .then(function(res) {
+//         const { joke } = res.data;
+
+//         return appendFileAsync("jokes.txt", joke + "\n");
+//     })
+//     .then(function() {
+//         return readFileAsync("jokes.txt", "utf8");
+//     })
+//     .then(function(data) {
+//         console.log("Saved dad jokes:");
+//         console.log(data);
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     });
+
+// getMovie();
+
+// async function getMovie() {
+//     try {
+//         const { movie } = await inquirer.prompt({
+//             message: "Search a movie:",
+//             name: "movie"
+//         });
+
+//         const { data } = await axios.get(
+//             `https://www.omdbapi.com/?t=${movie}&apikey=trilogy`
+//         );
+
+//         console.log(data);
+
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
 
 
